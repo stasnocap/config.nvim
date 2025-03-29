c33f33173a1cbe2334932733bb643fadaf0e1efd
stasnocap <stasnocap@gmail.com>
2025-03-29 17:14:38 +0300

    add oil

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false,
+  config = function ()
+    require('oil').setup()
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end
+}
