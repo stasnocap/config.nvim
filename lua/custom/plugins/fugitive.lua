10f25191b9318f7bc065049cb07d0242fa90e746
stasnocap <stasnocap@gmail.com>
2025-03-30 20:41:56 +0300

    remove gitsings, add some fugitive commands

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -11,6 +11,17 @@ return {
         vim.api.nvim_buf_set_keymap(0, 'n', 'u', 'k', { noremap = true, silent = true })
         vim.api.nvim_buf_set_keymap(0, 'n', 'e', 'j', { noremap = true, silent = true })
         vim.api.nvim_buf_set_keymap(0, 'n', 'i', 'l', { noremap = true, silent = true })
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { buffer = bufnr, remap = false }
+
+        vim.keymap.set('n', '<leader>p', function()
+          vim.cmd.Git 'push'
+        end, opts)
+
+        vim.keymap.set('n', '<leader>P', function()
+          vim.cmd 'Git pull --rebase'
+        end, opts)
       end,
     })
   end,
