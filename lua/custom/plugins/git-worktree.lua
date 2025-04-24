92963c63c9e928c2190ef886129c970a933feecd
stasnocap <stasnocap@gmail.com>
2025-04-24 16:21:43 +0300

    change worktree shortcut

--- lua/custom/plugins/git-worktree.lua
+++ lua/custom/plugins/git-worktree.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     local telescope = require('telescope')
     telescope.load_extension 'git_worktree'
-    vim.keymap.set('n', '<leader>ws', telescope.extensions.git_worktree.git_worktrees)
+    vim.keymap.set('n', '<leader>wt', telescope.extensions.git_worktree.git_worktrees)
     vim.keymap.set('n', '<leader>wa', telescope.extensions.git_worktree.create_git_worktree)
   end,
 }
