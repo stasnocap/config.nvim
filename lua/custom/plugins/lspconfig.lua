04a293e37619a58064afe8d6f8764ecf0931fd45
stasnocap <stasnocap@gmail.com>
2025-06-08 15:59:55 +0300

    automated dev commit

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -31,7 +31,7 @@ return {
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
           map('gT', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map('<C-K>', vim.lsp.buf.hover, 'Hover Documentation')
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
