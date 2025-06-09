d5d6d3388878d29837cd9613ed4d2796fd787d06
stasnocap <stasnocap@gmail.com>
2025-06-09 10:38:22 +0300

    automated dev commit

--- lua/custom/plugins/git-worktree.lua
+++ lua/custom/plugins/git-worktree.lua
@@ -1,9 +1,11 @@
 return {
-  'ThePrimeagen/git-worktree.nvim',
+  'polarmutex/git-worktree.nvim',
+  version = '^2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local telescope = require('telescope')
+    local telescope = require 'telescope'
     telescope.load_extension 'git_worktree'
-    vim.keymap.set('n', '<leader>ws', telescope.extensions.git_worktree.git_worktrees)
+    vim.keymap.set('n', '<leader>ws', telescope.extensions.git_worktree.git_worktree)
     vim.keymap.set('n', '<leader>wa', telescope.extensions.git_worktree.create_git_worktree)
   end,
 }
