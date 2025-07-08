10ce8c0c6f5805b4c0fbea4adc3c4c383d92517c
stasnocap <stasnocap@gmail.com>
2025-07-08 13:03:26 +0300

    automated dev commit

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'razor', 'c_sharp' },
       auto_install = true,
       highlight = {
         enable = true,
