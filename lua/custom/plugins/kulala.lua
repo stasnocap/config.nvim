03c20b4e737b6cdcfa48f0d85c611fb6a343b8c5
stasnocap <stasnocap@gmail.com>
2025-10-21 11:58:30 +0300

    automated dev commit

---
+++ lua/custom/plugins/kulala.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'mistweaverco/kulala.nvim',
+    ft = { 'http', 'rest' },
+    opts = {
+      -- your configuration comes here
+      global_keymaps = false,
+      global_keymaps_prefix = '<leader>h',
+      kulala_keymaps_prefix = '',
+    },
+  },
+}
