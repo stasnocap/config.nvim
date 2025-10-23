57113d56b3b5168101e83fe3d362f8dc8a5f7e23
stasnocap <stasnocap@gmail.com>
2025-10-23 09:12:48 +0300

    automated dev commit

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,7 +2,17 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    local catppuccin = require 'catppuccin.utils.lualine' 'latte'
+    catppuccin.normal.a.fg = catppuccin.normal.a.bg
+    catppuccin.insert.a.fg = catppuccin.insert.a.bg
+    catppuccin.visual.a.fg = catppuccin.visual.a.bg
+    catppuccin.replace.a.fg = catppuccin.replace.a.bg
+    catppuccin.command.a.fg = catppuccin.command.a.bg
+    catppuccin.inactive.a.fg = catppuccin.inactive.a.bg
     require('lualine').setup {
+      options = {
+        theme = catppuccin,
+      },
       sections = { lualine_c = { { 'filename', path = 1 } } },
     }
   end,
--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -1,3 +1,6 @@
 return {
   'xiyaowong/transparent.nvim',
+  config = function ()
+    require('transparent').clear_prefix('lualine')
+  end
 }
